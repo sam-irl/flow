a586ec90e566b7c86e8eb785f4c979976b4bd21f
sam-irl <therealsamave@gmail.com>
2017-08-11 20:22:53 -0500

    DEVELOP - Menus!

--- src/io/github/samirl/flow/Flow.java
+++ src/io/github/samirl/flow/Flow.java
@@ -1,14 +1,18 @@
 package io.github.samirl.flow;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
+import java.awt.event.ActionEvent;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
 public class Flow extends JFrame {
+	private JLabel statusbar;
 	public Flow() {
 		init();
 	}
@@ -17,11 +21,14 @@ public class Flow extends JFrame {
 		ImageIcon icon = new ImageIcon("icon.png");
 		setIconImage(icon.getImage());
 		setTitle("Go With The Flow - Debate Flowing Software");
-		setSize(300, 200);
+		setSize(900, 600);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	private void doMenu() {
+		statusbar = new JLabel("Go With The Flow v0.0.1-SNAPSHOT: LD Edition | Ready");
+		statusbar.setBorder(BorderFactory.createEtchedBorder());
+		add(statusbar, BorderLayout.SOUTH);
 		JMenuBar bar = new JMenuBar();
 		JMenu file = new JMenu("File");
 		JMenu save = new JMenu("Save");
@@ -30,7 +37,31 @@ public class Flow extends JFrame {
 		JMenuItem doc = new JMenuItem("Save as .doc");
 		JMenuItem pdf = new JMenuItem("Save as .pdf");
 		JMenuItem rtf = new JMenuItem("Save as .rtf");
-		
+		JMenuItem quit = new JMenuItem("Quit");
+		quit.addActionListener((ActionEvent e) -> {
+			System.exit(0);
+		});
+		save.add(doc);
+		save.add(pdf);
+		save.add(rtf);
+		file.add(newFile);
+		file.add(open);
+		file.addSeparator();
+		file.add(save);
+		file.addSeparator();
+		file.add(quit);
+		JMenu edit = new JMenu("Edit");
+		JMenuItem bold = new JMenuItem("Bold");
+		JMenuItem italic = new JMenuItem("Italic");
+		JMenuItem underline = new JMenuItem("Underline");
+		edit.add(bold);
+		edit.add(italic);
+		edit.add(underline);
+		JMenu flow = new JMenu("Flow");
+		bar.add(file);
+		bar.add(edit);
+		bar.add(flow);
+		setJMenuBar(bar);
 	}
 	public static void main(String[] args) {
 		EventQueue.invokeLater(() -> {
